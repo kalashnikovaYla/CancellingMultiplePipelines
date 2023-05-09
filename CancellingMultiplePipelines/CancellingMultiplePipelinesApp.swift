7edf4735cc5bfd87f22205ca51f893249a7f8956
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-09 20:44:38 +0500

    Initial Commit

---
+++ CancellingMultiplePipelines/CancellingMultiplePipelinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CancellingMultiplePipelinesApp.swift
+//  CancellingMultiplePipelines
+//
+//  Created by sss on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CancellingMultiplePipelinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
