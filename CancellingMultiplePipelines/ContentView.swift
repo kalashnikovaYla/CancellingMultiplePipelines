ab0553b0e3f6c45b6e0225da4c4387bd8f0c962f
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-09 22:56:13 +0500

    Add

--- CancellingMultiplePipelines/ContentView.swift
+++ CancellingMultiplePipelines/ContentView.swift
@@ -6,16 +6,68 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = CancellingMultiplePipelinesViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Group {
+            
+            HStack {
+                TextField("Enter name", text: $viewModel.firstName)
+                    .textFieldStyle(.roundedBorder)
+                Text(viewModel.firstNameValidation)
+            }
+            
+            HStack {
+                TextField("Enter lastName", text: $viewModel.lastName)
+                    .textFieldStyle(.roundedBorder)
+                Text(viewModel.lastNameValidation)
+            }
         }
         .padding()
+        
+        Button {
+            viewModel.cancelAllValidations()
+        } label: {
+            Text("Cancel all operations")
+        }
+
+    }
+}
+
+class CancellingMultiplePipelinesViewModel: ObservableObject {
+    
+    @Published var firstName: String = ""
+    @Published var firstNameValidation: String = ""
+    
+    @Published var lastName: String = ""
+    @Published var lastNameValidation: String = ""
+    
+    private var validationCancellables: Set<AnyCancellable> = []
+    
+    init() {
+        $firstName
+            .map{$0.isEmpty ? "🔐" : "💗"}
+            .sink { [unowned self] value in
+                self.firstNameValidation = value
+            }
+            .store(in: &validationCancellables)
+        
+        $lastName
+            .map{$0.isEmpty ? "🔐" : "💗"}
+            .sink { [unowned self] value in
+                self.lastNameValidation = value
+            }
+            .store(in: &validationCancellables)
+    }
+    
+    func cancelAllValidations() {
+        firstNameValidation = ""
+        lastNameValidation = "" 
+        validationCancellables.removeAll()
     }
 }
 
